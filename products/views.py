06c2bba2fabbaa3da45c6309ce1f0c2f70890be0
mbgoodguy <nerooren4001@gmail.com>
2023-05-22 02:51:06 +0300

    Check {% for %} work

--- products/views.py
+++ products/views.py
@@ -11,4 +11,27 @@ def index(request):
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - Каталог',
+        'products': [
+            {
+                'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6090,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+            },
+            {
+                'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
+                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+                'price': 3390,
+                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
+            },
+            {
+                'image': '/static/vendor/img/products/Black-Nike-Heritage-backpack.png',
+                'name': 'Черный рюкзак Nike Heritage',
+                'price': 2340,
+                'description': 'Плотная ткань. Легкий материал.',
+            }
+        ]
+    }
+    return render(request, 'products/products.html', context)
