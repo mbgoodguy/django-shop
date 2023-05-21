18f4f0672faef884fb68cce2a7b649c1909fb53d
mbgoodguy <nerooren4001@gmail.com>
2023-05-22 01:45:48 +0300

    Check {% %} tag work

--- products/views.py
+++ products/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Test Title',
+        'is_promotion': False,
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
