9fe74428bfe56483ea1ea573897a4a46ec3581a4
mbgoodguy <nerooren4001@gmail.com>
2023-05-30 10:24:32 +0300

    Add User model

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from products.models import Product, ProductCategory
+from users.models import User
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(ProductCategory)
+admin.site.register(User)
