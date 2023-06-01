250ab69497981b58c59c39d7385011ab3d251696
mbgoodguy <nerooren4001@gmail.com>
2023-06-01 11:30:18 +0300

    Add redirect to / after authorization

--- users/views.py
+++ users/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
 from users.models import User
 from users.forms import UserLoginForm
+from django.urls import reverse
 
 
 # Create your views here.
 def login(request):
-    context = {'form': UserLoginForm()}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
     return render(request, 'users/login.html', context)
 
 
