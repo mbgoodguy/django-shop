7dca68913e2c6af4729bb164878d7094f6a98e5b
mbgoodguy <nerooren4001@gmail.com>
2023-07-08 20:03:08 +0300

    Add settings for redis

--- products/urls.py
+++ products/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from products.views import ProductsListView, basket_add, basket_remove
 
@@ -7,6 +8,7 @@ app_name = 'products'
 urlpatterns = [
     path('', ProductsListView.as_view(), name='index'),
     path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),
+    path('category/<int:category_id>/', cache_page(30)(ProductsListView.as_view()), name='category'),
     path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
 
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
--- store/settings.py
+++ store/settings.py
@@ -83,10 +83,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'store.wsgi.application'
 
+# for django-debug-toolbar
 INTERNAL_IPS = [
     '127.0.0.1',
     'localhost'
 ]
+
+# for Redis
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
