9d0a6ff76fd3d6d2014019580c64a0a311ada1f7
mbgoodguy <nerooren4001@gmail.com>
2023-08-17 01:52:03 +0300

    Create Order and OrderForm models
    Add humanize tag for |intcomma using. It makes better sum and total sum representation

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from orders.models import Order
+
+
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'first_name',
+        'last_name',
+        'email',
+        'address',
+        'basket_history',
+        'created',
+        'initiator',
+        'status'
+    )
+    fields = (
+        'id',
+        'created',
+        ('first_name', 'last_name'),
+        ('email', 'address'),
+        'basket_history',
+        'initiator',
+        'status'
+    )
+    readonly_fields = ('id', 'created')
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+from users.models import User
+
+
 # Create your models here.
+class Order(models.Model):
+    STATUSES = [
+        ('1', 'Создан'),
+        ('2', 'Оплачен'),
+        ('3', 'В пути'),
+        ('4', 'Доставлен'),
+    ]
+
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.CharField(max_length=256)
+    address = models.CharField(max_length=256)
+    basket_history = models.JSONField(default=dict)
+    created = models.DateTimeField(auto_now_add=True)
+    initiator = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=6, choices=STATUSES, default='1')
+
+    def __str__(self):
+        return f'Order №{self.id}. {self.first_name} {self.last_name}'
